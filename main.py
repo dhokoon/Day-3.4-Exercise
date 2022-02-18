829d4099d5e81c277573c1c8ef870c4c8df9e164
dhokoon <52174999+dhokoon@users.noreply.github.com>
2022-02-18 05:20:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# print(type(size))
+# print(type(add_pepperoni))
+# print(type(extra_cheese))
+
+bill = 0
+size = size.upper()
+add_pepperoni = add_pepperoni.upper()
+extra_cheese = extra_cheese.upper()
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
+
+
+
